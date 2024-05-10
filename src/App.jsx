79679f5d7814658f732db6f5f36caf2728af6088
vinay <vinay@idreameducation.org>
2024-05-10 12:35:22 +0530

    modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 //import { useState } from 'react'
 import './App.css';
 import Form from './components/Form';
-//import ExcelReader from './components/ExcelSheetReader';
 
 function App() {
   return (
     <>
       <div className="appContainer">
         <Form />
-        {/*<ExcelReader />*/}
       </div>
     </>
   );
